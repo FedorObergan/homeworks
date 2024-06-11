c925b9698341e0b66a06a88ea116006ae2ecfb33
fedor_obergan <oberganfedor@mail.ru>
2024-06-11 08:26:24 +0300

    Change 2.1-databases/work_with_database

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from phones.models import Phone
+
+
 # Register your models here.
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', ]
+    prepopulated_fields = {'slug': ('name',)}
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,42 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_type = request.GET.get('sort')
+    records = []
+    phone_objects = Phone.objects.all()
+    for phone in phone_objects:
+        record = {
+            'name': phone.name,
+            'price': phone.price,
+            'image': phone.image,
+            'slug': phone.slug,
+        }
+        records.append(record)
+    if sort_type:
+        if sort_type == 'name':
+            cont = sorted(records, key=lambda x: x['name'])
+        elif sort_type == 'min_price':
+            cont = sorted(records, key=lambda x: x['price'])
+        else:
+            cont = sorted(records, key=lambda x: x['price'], reverse=True)
+    else:
+        cont = records
+    context = {
+        'phones': cont
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    right_phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': {
+            'name': right_phone.name,
+            'price': right_phone.price,
+            'image': right_phone.image,
+            'release_date': right_phone.release_date,
+            'lte_exists': right_phone.lte_exists,
+        }
+    }
     return render(request, template, context)
